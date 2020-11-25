6a0527a0d41f9b114d082ace45d217417b8417bb
Arhan Chaudhary <arhan.ch@gmail.com>
2020-11-24 16:19:38 -0800

    Added timewebapp skeleton as rootapp

--- timeweb/timeweb/urls.py
+++ timeweb/timeweb/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+# Enter the app name in following syntax for this to work 
+    path('', include('timewebapp.urls')),
 ]
