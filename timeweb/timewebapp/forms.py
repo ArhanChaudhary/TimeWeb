640e0617a01694d0e2030aefd08e99fba4638da4
Arhan Chaudhary <arhan.ch@gmail.com>
2020-11-27 12:23:41 -0800

    Connected Model to Form;Performed initial DB Migration; TODO: Connect Model to DB

--- timeweb/timewebapp/forms.py
+++ timeweb/timewebapp/forms.py
@@ -1,22 +1,22 @@
 # import form class from django 
 from django import forms 
   
-# import GeeksModel from models.py 
-# from .models import TimewebModel 
+# import TimewebModel from models.py 
+from .models import TimewebModel 
   
 # create a ModelForm 
-class TimewebForm(forms.Form): 
+class TimewebForm(forms.ModelForm): 
     
-    first_name = forms.CharField(max_length = 200)  
-    last_name = forms.CharField(max_length = 200)  
-    roll_number = forms.IntegerField(  
-                     help_text = "Enter 6 digit roll number"
-                     )  
-    password = forms.CharField(widget = forms.PasswordInput())  
+    # first_name = forms.CharField(max_length = 200)  
+    # last_name = forms.CharField(max_length = 200)  
+    # roll_number = forms.IntegerField(  
+    #                  help_text = "Enter 6 digit roll number"
+    #                  )  
+    # password = forms.CharField(widget = forms.PasswordInput())  
 
 
-  # specify the name of model to use 
-    # class Meta: 
-    #     model = TimewebModel 
-    #     fields = "__all__"
-    #     exclude = ["img", "last_modified"]
+    #specify the name of model to use 
+        class Meta: 
+            model = TimewebModel 
+            fields = "__all__"
+            #exclude = ["img", "last_modified"]
--- timeweb/timewebapp/models.py
+++ timeweb/timewebapp/models.py
@@ -1,16 +1,18 @@
-# # import the standard Django Model 
-# # from built-in library 
-# from django.db import models 
+# import the standard Django Model 
+# from built-in library 
+from django.db import models 
+from django.utils.timezone import now
+
    
-# # declare a new model with a name "TimewebModel" 
-# class TimewebModel(models.Model): 
-#         # fields of the model 
-#     title = models.CharField(max_length = 200) 
-#     description = models.TextField() 
-#     last_modified = models.DateTimeField() 
-#     img = models.ImageField(upload_to = "images/") 
+# declare a new model with a name "TimewebModel" 
+class TimewebModel(models.Model): 
+        # fields of the model 
+    title = models.CharField(max_length = 200) 
+    description = models.TextField() 
+    last_modified = models.DateTimeField(blank = True, default= now) 
+    img = models.ImageField(upload_to = "images/") 
    
-#         # renames the instances of the model 
-#         # with their title name 
-#     def __str__(self): 
-#         return self.title 
+        # renames the instances of the model 
+        # with their title name 
+    def __str__(self): 
+        return self.title 
