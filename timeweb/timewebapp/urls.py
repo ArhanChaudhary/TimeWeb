95300c61e099c163e01b9b5366d7ca0bdd9dac3c
Arhan Chaudhary <arhan.ch@gmail.com>
2020-11-29 16:36:36 -0800

    Fixed formatting changes after linter

--- timeweb/timewebapp/urls.py
+++ timeweb/timewebapp/urls.py
@@ -1,8 +1,10 @@
+"""[summary]
+"""
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.home_view),
-    path('list/',views.list_view),
+    path('list/', views.list_view),
 ]
--- timeweb/timewebapp/views.py
+++ timeweb/timewebapp/views.py
@@ -1,23 +1,25 @@
-from django.shortcuts import render 
+from django.shortcuts import render
 from .forms import TimewebForm, TimewebFormList
 from .models import TimewebModel
-# creating a home view 
-def home_view(request): 
+# creating a home view
 
-    # dictionary for initial data with  
-    # field names as keys 
-    context = {} 
 
-    # add the dictionary during initialization 
-    form = TimewebForm(request.POST or None, request.FILES or None) 
+def home_view(request):
+
+    # dictionary for initial data with
+    # field names as keys
+    context = {}
+
+    # add the dictionary during initialization
+    form = TimewebForm(request.POST or None, request.FILES or None)
     context['form'] = form
 
-    # check if form data is valid 
-    if form.is_valid(): 
+    # check if form data is valid
+    if form.is_valid():
         print("Form Values::")
         print(request.POST)
-        
-        # save the form data to model 
+
+        # save the form data to model
         save_data = form.save(commit=False)
         print("---")
         print(save_data.description)
@@ -26,19 +28,23 @@ def home_view(request):
         save_data.save()
         print("Database saved")
 
-    return render(request, "home.html", context) 
+    return render(request, "home.html", context)
 
-def list_view(request): 
 
-    # dictionary for initial data with  
-    # field names as keys 
+def list_view(request):
+
+    # dictionary for initial data with
+    # field names as keys
     context = {}
-    form = TimewebFormList(request.POST or None, request.FILES or None) 
+    form = TimewebFormList(request.POST or None, request.FILES or None)
     context['form'] = form
-    context['allobj'] = 'All Objects:\n'+'\n'.join(obj.__str__() for obj in TimewebModel.objects.all())
+    context['allobj'] = 'All Objects:\n' + \
+        '\n'.join(obj.__str__() for obj in TimewebModel.objects.all())
     if form.is_valid():
         save_data = form.save(commit=False)
-        selected_obj = TimewebModel.objects.get(pk=save_data.enter_id_to_delete)
+        selected_obj = TimewebModel.objects.get(
+            pk=save_data.enter_id_to_delete)
         selected_obj.delete()
-        context['allobj'] = 'All Objects:\n'+'\n'.join(obj.__str__() for obj in TimewebModel.objects.all())
-    return render(request, "home_list.html", context) 
+        context['allobj'] = 'All Objects:\n' + \
+            '\n'.join(obj.__str__() for obj in TimewebModel.objects.all())
+    return render(request, "home_list.html", context)
