e9b8f830b42ea3b4b6ae4d727528819b61d5ef13
Arhan Chaudhary <arhan.ch@gmail.com>
2020-12-24 22:56:14 -0800

    Revamped UI

--- timeweb/timewebapp/urls.py
+++ timeweb/timewebapp/urls.py
@@ -5,7 +5,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.TimewebView.as_view(),name='home'),
-    path('', views.TimewebListView.as_view(),name='lists'),
+    path('<int:pk>/', views.TimewebView.as_view(),name='list'),
+    path('', views.TimewebListView.as_view(),name='home'),
     path('new/', views.TimewebView.as_view(),name='new'),
+    path('graph/<int:pk>/', views.TimewebGraphView.as_view(),name='graph'),
 ]
--- timeweb/timewebapp/views.py
+++ timeweb/timewebapp/views.py
@@ -5,8 +5,6 @@ from .models import TimewebModel
 from .forms import TimewebForm
 import logging # import the logging library
 
-coords = ''
-
 class TimewebView(View):
 
     # Get an instance of a logger
@@ -16,28 +14,28 @@ class TimewebView(View):
         self.context = {}
 
     def make_form_instance(self,request,pk):
+
+        # Creates form after user enters "New" 
         if pk == None:
             self.form = TimewebForm(request.POST or None, request.FILES or None)
             self.context['submit'] = 'Create'
-            self.context['delete'] = 'Cancel'
-            self.context['form'] = self.form
         else:
             self.form = TimewebForm(request.POST or None, request.FILES or None,initial={
                 'title':get_object_or_404(TimewebModel, pk=pk).title,
                 'description':get_object_or_404(TimewebModel, pk=pk).description,
                 })
-            self.context['submit'] = 'Update'
-            self.context['delete'] = 'Delete'
-            self.context['form'] = self.form
+            self.context['submit'] = 'Submit'
+        self.context['form'] = self.form
+        self.context['pk'] = pk
 
+    # User enters "New"
     def get(self,request,pk=None):
+        
         self.make_form_instance(request,pk)
-        self.logger.debug(self.context)
         return render(request, "home.html", self.context)
 
     def post(self,request,pk=None):
         self.make_form_instance(request,pk)
-        self.logger.debug(self.context)
         if self.form.is_valid() and 'Submitbutton' in request.POST:
             if pk == None: # Handle "new"
                 save_data = self.form.save(commit=False)
@@ -52,11 +50,6 @@ class TimewebView(View):
                 save_data.save()
                 self.logger.debug("Updated")
             return redirect('../')
-        elif 'Deletebutton' in request.POST:
-                selected_form = get_object_or_404(TimewebModel, pk=pk)
-                selected_form.delete()
-                self.logger.debug("Deleted")
-                return redirect('../')
         else:
             self.logger.debug("Invalid Form")
             return render(request, "home.html", self.context)
@@ -74,13 +67,26 @@ class TimewebListView(View):
 
     def get(self,request):
         self.make_list()
-
-        global coords
-        self.context['coords'] = coords
-        print(self.context['coords'])
-        return render(request, "home_list.html", self.context)
-
+        return render(request, "list.html", self.context)
+    
+    # An example of request.POST after delete is pressed:
+    # <QueryDict: {'title': ['yyy'], 'description': ['test'], 'Deletebutton': ['']}>
+    # As you can see, the name of the submit button is passed on to request.POST
+    # However, it still cannot be referanced because 'Deletebutton': [''] is not a good indication
+    # So, pass in a value into the button such that the new request.POST will have Deletebutton': ['deleted'] instead
     def post(self,request):
-        global coords
-        coords = request.POST['coords']
-        return redirect(".")
+        for key, value in request.POST.items():
+            if value == "deleted":
+                pk = key
+                selected_form = get_object_or_404(TimewebModel, pk=pk)
+                selected_form.delete()
+                self.logger.debug("Deleted")
+                self.make_list()
+        return render(request, "list.html", self.context)
+
+class TimewebGraphView(View):
+    context = {}
+    logger = logging.getLogger(__name__)
+
+    def get(self,request,pk):
+        return render(request, "graph.html", self.context)
