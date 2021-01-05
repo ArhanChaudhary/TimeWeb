65023cbe44060336837aa648da0fec61a50c9e61
Arhan Chaudhary <arhan.ch@gmail.com>
2021-01-05 00:53:27 -0800

    Added AJAX Post

--- timeweb/timewebapp/urls.py
+++ timeweb/timewebapp/urls.py
@@ -1,12 +1,15 @@
 """[summary]
 """
 from django.urls import path
+from django.views.generic import RedirectView
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.TimewebView.as_view(),name='list'),
-    path('', views.TimewebListView.as_view(),name='home'),
-    path('new/', views.TimewebView.as_view(),name='new'),
-    path('graph/<int:pk>/', views.TimewebGraphView.as_view(),name='graph'),
+    path(r'<int:pk>/', views.TimewebView.as_view(),name='list'),
+    path(r'', views.TimewebListView.as_view(),name='home'),
+    path(r'new/', views.TimewebView.as_view(),name='new'),
+    path(r'graph/<int:pk>/', views.TimewebGraphView.as_view(),name='graph'),
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/favicon.ico')),
 ]
