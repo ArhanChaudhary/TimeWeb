131baf009c8dfd10bc3b5b3e400fd497e2d4bc4c
Arhan Chaudhary <arhan.ch@gmail.com>
2021-01-16 19:17:04 -0800

    Accidentally leaked secret key; changed and ignored on commit

--- timeweb/timeweb/settings.py
+++ timeweb/timeweb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .SECRET_KEY import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -18,15 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%91@z+o5#j!fc+#frghnqxkwz^bge!qz)e^3#1311x!tspz)g&'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+print(SECRET_KEY)
 # Application definition
 
 INSTALLED_APPS = [
