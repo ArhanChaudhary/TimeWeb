e7db774583bcdf15d52b0c92062977a738a703e3
Arhan Chaudhary <arhan.ch@gmail.com>
2021-08-19 23:05:24 -0700

    Added note label template to user guide

--- timeweb/timewebapp/static/js/user-guide.js
+++ timeweb/timewebapp/static/js/user-guide.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    $(".question-container").append($("#question-icon-template").html());
+    const label_icon_template = $("#label-icon-template").html();
+    $(".label-question, .label-note").each(function() {
+        const label_icon = $(label_icon_template);
+        if ($(this).hasClass("label-question")) {
+            label_icon.find(".label-icon").attr("src", "/static/images/status-icons/question-mark.png");
+        } else if ($(this).hasClass("label-note")) {
+            label_icon.find(".label-icon").attr("src", "/static/images/status-icons/exclamation-mark.png");
+        }
+        $(this).append(label_icon);
+    });
 });
