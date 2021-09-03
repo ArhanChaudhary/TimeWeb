fbcfaf4d0cc68fc1e2b1ae0dbc0d2379582ff1e9
Arhan Chaudhary <arhan.ch@gmail.com>
2021-09-03 16:37:25 -0700

    Merged user guide notes with questions

--- timeweb/timewebapp/static/js/user-guide.js
+++ timeweb/timewebapp/static/js/user-guide.js
@@ -1,13 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const label_icon_template = $("#label-icon-template").html();
-    $(".label-question, .label-note").each(function() {
-        const label_icon = $(label_icon_template);
-        if ($(this).hasClass("label-question")) {
-            label_icon.find(".label-icon").attr("src", "/static/images/status-icons/question-mark.png");
-        } else if ($(this).hasClass("label-note")) {
-            label_icon.find(".label-icon").attr("src", "/static/images/status-icons/exclamation-mark.png");
-        }
-        $(this).append(label_icon);
+    $(".label-question").each(function() {
+        $(this).append($(label_icon_template));
     });
     const table_of_contents_major_category_template = $("#table-of-contents-major-category-template").html();
     const table_of_contents_minor_category_template = $("#table-of-contents-minor-category-template").html();
@@ -34,14 +28,5 @@ document.addEventListener("DOMContentLoaded", function() {
         minor_category_li.find("a").attr("href", `#${$label_question.attr("id")}`).text($label_question.find(".label-title").text());
 
         $("#table-of-contents-container #category-user-guide-labels").append(minor_category_li);
-    });
-
-    $(".label-note").each(function() {
-        const $label_note = $(this);
-
-        const minor_category_li = $(table_of_contents_minor_category_template);
-        minor_category_li.find("a").attr("href", `#${$label_note.attr("id")}`).text($label_note.find(".label-title").text());
-
-        $("#table-of-contents-container #category-user-guide-notes").append(minor_category_li);
     });
 });
