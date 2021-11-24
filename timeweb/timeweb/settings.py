88d4e12f542c8fa7d4f9bcacb0c8d5aefea1a3a5
Arhan Chaudhary <arhan.ch@gmail.com>
2021-11-24 13:48:10 -0800

    First part of allauth settings

--- timeweb/timeweb/settings.py
+++ timeweb/timeweb/settings.py
@@ -81,6 +81,11 @@ INSTALLED_APPS = [
     'pwa',
     'colorfield',
     'django_cleanup.apps.CleanupConfig',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -112,8 +117,12 @@ TEMPLATES = [
         },
     },
 ]
-# Redirect to home URL after login
+
+SITE_ID = 2
+
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
 WSGI_APPLICATION = 'timeweb.wsgi.application'
 
 MAX_UPLOAD_SIZE = 5242880 # 40 MiB (max background image size)
@@ -239,4 +248,19 @@ LOGGING = {
             'style': '{',
         },
     },
+}
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'offline',
+        }
+    }
 }
