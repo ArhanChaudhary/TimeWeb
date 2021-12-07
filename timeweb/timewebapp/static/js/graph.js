5fc0ce5bad84960cbf248f22a667bd36badac16c
Arhan Chaudhary <arhan.ch@gmail.com>
2021-12-06 20:37:06 -0800

    Displayed estimated completion time for tomorrow

--- timeweb/timewebapp/static/js/graph.js
+++ timeweb/timewebapp/static/js/graph.js
@@ -565,7 +565,7 @@ class VisualAssignment extends Assignment {
 
             if (displayed_day.valueOf() !== date_now.valueOf()) {
                 center(str_day, 1);
-                center(`${pluralize(this.sa.unit, 2)} to Complete for this Day: ${todo}`, 2);
+                center(`${pluralize(this.sa.unit, 2)} to Complete for this Day: ${todo} ${utils.formatting.formatMinutes(todo * this.sa.time_per_unit)}`, 2);
             }
         }
         screen.scale(1 / this.scale, 1 / this.scale);
