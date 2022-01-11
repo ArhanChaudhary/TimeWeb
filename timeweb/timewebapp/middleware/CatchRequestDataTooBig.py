7202dd70972e20c4901e4d3d607d2a67d17073d6
Arhan Chaudhary <arhan.ch@gmail.com>
2022-01-10 21:35:34 -0800

    Remove hardcode in middleware

--- timeweb/timewebapp/middleware/CatchRequestDataTooBig.py
+++ timeweb/timewebapp/middleware/CatchRequestDataTooBig.py
@@ -1,12 +1,13 @@
 from django.core.exceptions import RequestDataTooBig
 from django.http.response import HttpResponse
+from django.urls import reverse
 
 class CatchRequestDataTooBig(object):
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.path != "/settings":
+        if request.path != reverse("settings"):
             try:
                 request.body
             except RequestDataTooBig:
