7aa9ae8d6f966c849b6ed02bd64e64559f9937b5
Arhan Chaudhary <arhan.ch@gmail.com>
2022-01-12 14:19:16 -0800

    Fixed service worker not properly caching assets

--- timeweb/timewebapp/static/js/serviceworker.js
+++ timeweb/timewebapp/static/js/serviceworker.js
@@ -1,7 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.2/workbox-sw.js');
 
 workbox.routing.registerRoute(
-    /\.(?:css|js|woff2|ttf|png|jpeg|svg|html|xml)$/,
+    /.*/,
     new workbox.strategies.StaleWhileRevalidate({
         cacheName: 'twcache',
         plugins: [
