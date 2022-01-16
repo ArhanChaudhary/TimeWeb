158c13bc79001ec22cda200716e2c6788fee2601
Arhan Chaudhary <arhan.ch@gmail.com>
2022-01-16 11:17:12 -0800

    Serve cdn libraries with a CacheFirst strategy

--- timeweb/timewebapp/static/js/serviceworker.js
+++ timeweb/timewebapp/static/js/serviceworker.js
@@ -1,7 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.2/workbox-sw.js');
 
 workbox.routing.registerRoute(
-    ({ request }) => request.destination !== "document",
+    ({ request }) => request.destination !== "document" && !request.url.startsWith("https://cdn"),
     new workbox.strategies.StaleWhileRevalidate({
         cacheName: 'twcache',
         plugins: [
@@ -10,6 +10,22 @@ workbox.routing.registerRoute(
             }),
             new workbox.expiration.ExpirationPlugin({
                 maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 7, // 7 Days
+            }),
+        ],
+    }),
+);
+
+workbox.routing.registerRoute(
+    ({ request }) => request.url.startsWith("https://cdn"),
+    new workbox.strategies.CacheFirst({
+        cacheName: 'twcdncache',
+        plugins: [
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 100,
                 maxAgeSeconds: 60 * 60 * 24 * 7, // 7 Days
             }),
         ],
