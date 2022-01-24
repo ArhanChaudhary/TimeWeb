8453437cc210c44b2e76fa068f9d14761cd069c5
Arhan Chaudhary <arhan.ch@gmail.com>
2022-01-24 10:48:34 -0800

    Added commented out code for cache control headers to bust the memory cache and use the service worker cache instead

--- timeweb/timewebapp/urls.py
+++ timeweb/timewebapp/urls.py
@@ -52,6 +52,15 @@ if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+    # # https://stackoverflow.com/questions/69301677/script-file-is-served-from-memory-cache-when-using-service-worker-on-chrome (google's crc323c module may fix this, come back to this in v1.7.1)
+    # # Run this with manage.py --nostatic
+    # from django.contrib.staticfiles.views import serve
+    # def custom_serve(request, path, insecure=False, **kwargs):
+    #     response = serve(request, path, insecure=True)
+    #     response['Cache-Control'] = 'no-store' (?) or something else
+    #     return response
+    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, view=custom_serve)
+
 allauth_urls += [
     path('username/reset', views.UsernameResetView.as_view(), name='reset_username'),
 ]
