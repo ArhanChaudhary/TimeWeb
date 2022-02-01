4ea2d971e740460dee4a7b375800b298f6ea3f90
Arhan Chaudhary <arhan.ch@gmail.com>
2022-02-01 13:39:30 -0800

    Organized static url redirects

--- timeweb/timewebapp/urls.py
+++ timeweb/timewebapp/urls.py
@@ -6,6 +6,9 @@ from django.conf import settings
 from allauth.account.urls import urlpatterns as allauth_urls
 from django.views.defaults import page_not_found
 
+def get_static_url(url_path):
+    return f"/static/{url_path}" if settings.DEBUG else f'https://storage.googleapis.com/twstatic/{url_path}'
+
 urlpatterns = [
     path('', views.TimewebView.as_view(),name='home'),
     path('settings', views.SettingsView.as_view(),name='settings'),
@@ -14,11 +17,11 @@ urlpatterns = [
     path('changelog', views.ChangelogView.as_view(),name='changelog'),
     path('example', views.ExampleAccountView.as_view(), name='example'),
     path('contact', views.ContactFormView.as_view(form_class=forms.ContactForm), name='contact_form'),
-    path('policies', RedirectView.as_view(url='https://storage.googleapis.com/twstatic/policies/policies.html'), name='policies'),
-    path('cookies', RedirectView.as_view(url='https://storage.googleapis.com/twstatic/policies/cookies.html')),
-    path('disclaimer', RedirectView.as_view(url='https://storage.googleapis.com/twstatic/policies/disclaimer.html')),
-    path('privacy', RedirectView.as_view(url='https://storage.googleapis.com/twstatic/policies/privacy.html')),
-    path('terms', RedirectView.as_view(url='https://storage.googleapis.com/twstatic/policies/terms.html')),
+    path('policies', RedirectView.as_view(url=get_static_url('policies/policies.html')), name='policies'),
+    path('cookies', RedirectView.as_view(url=get_static_url('policies/cookies.html'))),
+    path('disclaimer', RedirectView.as_view(url=get_static_url('policies/disclaimer.html'))),
+    path('privacy', RedirectView.as_view(url=get_static_url('policies/privacy.html'))),
+    path('terms', RedirectView.as_view(url=get_static_url('policies/terms.html'))),
     path('discord', RedirectView.as_view(url='https://discord.gg/fQgPXX5qpD'), name="discord"),
     path('gc-api-auth-init', views.GCOAuthView.as_view()),
     path('gc-api-auth-callback', views.GCOAuthView.as_view()),
@@ -28,16 +31,16 @@ urlpatterns = [
 Expires: 2023-07-27T07:00:00.000Z
 Preferred-Languages: en
 Canonical: https://timeweb.io/.well-known/security.txt''', content_type="text/plain"), name="security_file"),
-    path('android-chrome-192x192.png', RedirectView.as_view(url='https://storage.googleapis.com/twstatic/images/icons/android-chrome-192x192.png')),
-    path('android-chrome-512x512.png', RedirectView.as_view(url='https://storage.googleapis.com/twstatic/images/icons/android-chrome-512x512.png')),
-    path('apple-touch-icon-precomposed.png', RedirectView.as_view(url='https://storage.googleapis.com/twstatic/images/icons/apple-touch-icon-precomposed.png')),
-    path('apple-touch-icon.png', RedirectView.as_view(url='https://storage.googleapis.com/twstatic/images/icons/apple-touch-icon.png')),
-    path('browserconfig.xml', RedirectView.as_view(url='https://storage.googleapis.com/twstatic/images/icons/browserconfig.xml')),
-    path('favicon-16x16.png', RedirectView.as_view(url='https://storage.googleapis.com/twstatic/images/icons/favicon-16x16.png')),
-    path('favicon-32x32.png', RedirectView.as_view(url='https://storage.googleapis.com/twstatic/images/icons/favicon-32x32.png')),
-    path('favicon.ico', RedirectView.as_view(url='https://storage.googleapis.com/twstatic/images/icons/favicon.ico')),
-    path('mstile-150x150.png', RedirectView.as_view(url='https://storage.googleapis.com/twstatic/images/icons/mstile-150x150.png')),
-    path('safari-pinned-tab.svg', RedirectView.as_view(url='https://storage.googleapis.com/twstatic/images/icons/safari-pinned-tab.svg')),
+    path('android-chrome-192x192.png', RedirectView.as_view(url=get_static_url('images/icons/android-chrome-192x192.png'))),
+    path('android-chrome-512x512.png', RedirectView.as_view(url=get_static_url('images/icons/android-chrome-512x512.png'))),
+    path('apple-touch-icon-precomposed.png', RedirectView.as_view(url=get_static_url('images/icons/apple-touch-icon-precomposed.png'))),
+    path('apple-touch-icon.png', RedirectView.as_view(url=get_static_url('images/icons/apple-touch-icon.png'))),
+    path('browserconfig.xml', RedirectView.as_view(url=get_static_url('images/icons/browserconfig.xml'))),
+    path('favicon-16x16.png', RedirectView.as_view(url=get_static_url('images/icons/favicon-16x16.png'))),
+    path('favicon-32x32.png', RedirectView.as_view(url=get_static_url('images/icons/favicon-32x32.png'))),
+    path('favicon.ico', RedirectView.as_view(url=get_static_url('images/icons/favicon.ico'))),
+    path('mstile-150x150.png', RedirectView.as_view(url=get_static_url('images/icons/mstile-150x150.png'))),
+    path('safari-pinned-tab.svg', RedirectView.as_view(url=get_static_url('images/icons/safari-pinned-tab.svg'))),
 
     path('accounts/logout', page_not_found),
 
