a0c92921f4819eb3bdaecb7d604425056b84c3ef
Arhan Chaudhary <arhan.ch@gmail.com>
2022-02-07 02:22:17 -0800

    Forgot to completely implement b54d4de

--- timeweb/timewebapp/static/js/changelog.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    $("#doc-container").css("counter-reset", `major-category-counter ${$(".major-category").length + 1}`);
-    $(".major-category").each(function() {
-        const $major_category = $(this); 
-    })
-});
