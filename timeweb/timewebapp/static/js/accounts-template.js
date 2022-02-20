255440e1982ba741f146c38812d9ad79b4547a05
Arhan Chaudhary <arhan.ch@gmail.com>
2022-02-19 19:47:19 -0800

    Redesigned login screen

--- timeweb/timewebapp/static/js/accounts-template.js
+++ timeweb/timewebapp/static/js/accounts-template.js
@@ -1,10 +1,4 @@
 $(function() {
-    // Position login
-    $("main").css({
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-    })[0].style.setProperty("overflow", "hidden", "important");
     $("#form-wrapper form").submit(function() {
         $("#submit-button").attr("disabled", true);
     });
