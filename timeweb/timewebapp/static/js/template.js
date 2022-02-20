7497dcd8a8f1d42b52cfbf713b43762666e7aa47
Arhan Chaudhary <arhan.ch@gmail.com>
2022-02-19 19:42:51 -0800

    Created jquery-extensions.js to run on every view

--- timeweb/timewebapp/static/js/template.js
+++ timeweb/timewebapp/static/js/template.js
@@ -217,44 +217,6 @@ jconfirm.defaults = {
     closeAnimation: 'scale',
     animateFromElement: false,
 };
-$.fn.reverse = Array.prototype.reverse;
-// Info tooltip
-$.fn.info = function(facing,text,position) {
-    const info_button = $($("#info-button-template").html());
-    info_button.find(".info-button-text").addClass(`info-${facing}`).text(text);
-    info_button.click(() => false);
-    switch (position) {
-        case "prepend":
-            return info_button.prependTo(this);
-        case "after":
-            return info_button.insertAfter(this);
-        default:
-            return info_button.appendTo(this);
-    }
-}
-$.fn.hasOverflown = function() {
-    const e = this[0];
-    if (!e) return false;
-    return e.scrollHeight > e.clientHeight || e.scrollWidth > e.clientWidth;
-}
-$.fn.isFullyScrolled = function(params={}) {
-    const e = this[0];
-    return e.scrollTop + e.clientHeight + params.leeway >= e.scrollHeight;
-}
-$.fn.expandableTextareaHeight = function() {
-    $(this).on("input", function() {
-        $(this).css("height", "auto"); // Needed for expanding with text
-        $(this).css("height", $(this).prop("scrollHeight") + parseFloat($(this).css("padding-top")));
-    });
-    return $(this);
-}
-// Only change text
-$.fn.onlyText = function(text) {
-    $(this).contents().filter(function() {
-        return this.nodeType === Node.TEXT_NODE;
-    }).first()[0].nodeValue = text;
-    return $(this);
-};
 mathUtils = {
     // https://stackoverflow.com/questions/1458633/how-to-deal-with-floating-point-number-precision-in-javascript
     precisionRound: function(number, precision) {
