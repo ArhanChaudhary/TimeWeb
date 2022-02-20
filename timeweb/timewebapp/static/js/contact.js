fd1f51b770ab730ede8fc4d1dd8ef4a06dc73107
Arhan Chaudhary <arhan.ch@gmail.com>
2022-02-19 21:06:15 -0800

    Moved contact.js to inline script

--- timeweb/timewebapp/static/js/contact.js
+++
@@ -1,6 +0,0 @@
-$(window).one("load", function() {
-    $("#id_body").expandableTextareaHeight().trigger("input");
-})
-function submitContactForm(token) {
-    document.getElementById("contact-form").submit();
-}
