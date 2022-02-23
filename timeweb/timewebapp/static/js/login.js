5c5461b2bf63fc92ce70b661013ecd28beac7bc3
Arhan Chaudhary <arhan.ch@gmail.com>
2022-02-22 23:42:16 -0800

    Replace #example-message with a /example anchor

--- timeweb/timewebapp/static/js/login.js
+++ timeweb/timewebapp/static/js/login.js
@@ -4,12 +4,6 @@ $(function() {
     sessionStorage.removeItem("open_assignments");
     localStorage.removeItem("scroll");
 
-    $("#example-message").click(function() {
-        $("#id_login").val("timeweb@example.com");
-        $("#id_password").val("exampleaccount");
-        $("#submit-button").click();
-    });
-
     $(".socialaccount-container").click(function() {
         document.location.href = $(this).find("a").attr("href");
     });
