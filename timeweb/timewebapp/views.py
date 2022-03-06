e49ed164f7d62cb40f088989efa2df586e3f38c1
Arhan Chaudhary <arhan.ch@gmail.com>
2022-03-06 10:41:12 -0800

    Fixed google classroom integration not having a green box shadow after adddding google classroom assignments

--- timeweb/timewebapp/views.py
+++ timeweb/timewebapp/views.py
@@ -297,8 +297,9 @@ class TimewebView(LoginRequiredMixin, TimewebGenericView):
         if not self.settings_model.seen_latest_changelog:
             self.context['latest_changelog'] = CHANGELOGS[0]
 
+        self.context['enabled_gc_api'] = 'token' in self.settings_model.oauth_token
         if not request.session.get("already_created_gc_assignments_from_frontend", False):
-            self.context['creating_gc_assignments_from_frontend'] = 'token' in self.settings_model.oauth_token
+            self.context['creating_gc_assignments_from_frontend'] = self.context['enabled_gc_api']
         else:
             del request.session["already_created_gc_assignments_from_frontend"]
 
