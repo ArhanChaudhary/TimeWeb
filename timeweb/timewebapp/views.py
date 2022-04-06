ca7a1bced76c40bf97a697327c2f68e0ac8e464e
Arhan Chaudhary <arhan.ch@gmail.com>
2022-04-06 02:45:52 -0700

    Pass in settings model into context instead of individual settings

--- timeweb/timewebapp/views.py
+++ timeweb/timewebapp/views.py
@@ -209,7 +209,7 @@ class SettingsView(LoginRequiredMixin, TimewebGenericView):
         }
         self.context['form'] = SettingsForm(initial=initial)
 
-        self.context['settings_object'] = self.settings_model
+        self.context['settings_model'] = self.settings_model
         logger.info(f'User \"{request.user}\" is now viewing the settings page')
         return super().get(request)
         
@@ -285,20 +285,18 @@ class TimewebView(LoginRequiredMixin, TimewebGenericView):
 
     def add_user_models_to_context(self, request):
         self.context['assignment_models'] = self.assignment_models
+        self.context['settings_model'] = self.settings_model
         self.context['assignment_models_as_json'] = list(self.assignment_models.values())
         self.context['settings_model_as_json'] = model_to_dict(self.settings_model)
 
         del self.context['settings_model_as_json']['background_image'] # background_image isnt json serializable
         self.context['settings_model_as_json']['timezone'] = str(self.context['settings_model_as_json']['timezone'] or '') # timezone isnt json serializable
-        self.context['background_image'] = self.settings_model.background_image
-        self.context['horizontal_tag_position'] = self.settings_model.horizontal_tag_position
 
         if not self.settings_model.seen_latest_changelog:
             self.context['latest_changelog'] = CHANGELOGS[0]
 
-        self.context['enabled_gc_api'] = 'token' in self.settings_model.oauth_token
         if not request.session.get("already_created_gc_assignments_from_frontend", False):
-            self.context['creating_gc_assignments_from_frontend'] = self.context['enabled_gc_api']
+            self.context['creating_gc_assignments_from_frontend'] = 'token' in self.settings_model.oauth_token
         else:
             del request.session["already_created_gc_assignments_from_frontend"]
 
