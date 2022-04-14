b14b00009e87c20595941c98ad1146f8c7dc656f
Arhan Chaudhary <arhan.ch@gmail.com>
2022-04-14 12:29:56 -0700

    display password reset email message when resetting password

--- timeweb/timewebauth/urls.py
+++ timeweb/timewebauth/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('accounts/logout', page_not_found),
+    path("password/reset/", views.MessagedPasswordResetView.as_view(), name="account_reset_password"),
     path('username/reset', views.UsernameResetView.as_view(), name='reset_username'),
 ]
