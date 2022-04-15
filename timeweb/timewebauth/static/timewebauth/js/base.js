7d3e6b6e564b060c9a44290bc4b502dcd99b372d
Arhan Chaudhary <arhan.ch@gmail.com>
2022-04-15 01:02:00 -0700

    Rework code for login screen bubble animation

--- timeweb/timewebauth/static/timewebauth/js/base.js
+++ timeweb/timewebauth/static/timewebauth/js/base.js
@@ -8,7 +8,7 @@ $(window).one("load", function() {
         $("#submit-button").attr("disabled", true);
     });
 
-    let number_of_circles = $("#circles-background .circle").length;
+    let number_of_circles = $("#circles-background .bubble-up").length;
     $("#circles-background").prop("style").setProperty("--highest-circle-number", number_of_circles - 1);
 
     let vws = new Array(number_of_circles);
@@ -29,13 +29,9 @@ $(window).one("load", function() {
         b: 156
     };
 
-    const initial_sin_percentage = -0.5;
-    for (let i = 0; i < 1.01; i += 0.01) {
-        $("#circles-background").prop("style").setProperty(`--${Math.round(i*100)}`, Math.sin((i + initial_sin_percentage)*Math.PI));
-    }
-    $("#circles-background .circle").each(function(i) {
+    $("#circles-background .bubble-up").each(function(i) {
         let background_gradient_factor = 1 - i / (number_of_circles - 1);
-        $(this).css("background", `rgb(${front_color.r + background_gradient_factor * (back_color.r - front_color.r)}, ${front_color.g + background_gradient_factor * (back_color.g - front_color.g)}, ${front_color.b + background_gradient_factor * (back_color.b - front_color.b)})`);
+        $(this).children(".bubble-right").css("background", `rgb(${front_color.r + background_gradient_factor * (back_color.r - front_color.r)}, ${front_color.g + background_gradient_factor * (back_color.g - front_color.g)}, ${front_color.b + background_gradient_factor * (back_color.b - front_color.b)})`);
         this.style.setProperty("--circle-number", i);
         this.style.setProperty("--random-delay", `${Math.floor(Math.random()*11-5)}s`);
         this.style.setProperty("--random-x", `${Math.random()*6+5}vw`);
@@ -45,7 +41,7 @@ $(window).one("load", function() {
     if (sessionStorage.getItem("low-detail-mode")) {
         $("#circles-background").css("filter", "none");
     } else {
-        $("#circles-background .circle").first().on("animationstart", function() {
+        $("#circles-background .bubble-right").first().on("animationstart", function() {
             var get_frames = true;
             var judgement_time = 250;
             var framerate_threshold = 45;
