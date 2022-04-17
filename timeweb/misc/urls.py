99b789ff4adaea2f38005ef51ce6beb6153bb29c
Arhan Chaudhary <arhan.ch@gmail.com>
2022-04-16 18:14:41 -0700

    Move icons dir from timewebapp to misc

--- timeweb/misc/urls.py
+++ timeweb/misc/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, re_path
 from django.http import HttpResponse
 from . import views
+from django.views.generic import RedirectView
+from timewebapp.urls import get_static_url
+
+def app_static(url_path):
+    return get_static_url(__package__, url_path)
 
 urlpatterns = [
     path('robots.txt', lambda x: HttpResponse("# If you came from the discord gg you get a super duper secret role\n# pm me this message at Arch#5808\n# also, pls don't tell anyone as it'll ruin the fun of this small game\nUser-Agent: *\nDisallow:", content_type="text/plain"), name="robots_file"),
@@ -8,6 +13,17 @@ urlpatterns = [
 Expires: 2023-07-27T07:00:00.000Z
 Preferred-Languages: en
 Canonical: https://timeweb.io/.well-known/security.txt''', content_type="text/plain"), name="security_file"),
+
+    path('android-chrome-192x192.png', RedirectView.as_view(url=app_static('icons/android_chrome_192x192.png'))),
+    path('android-chrome-512x512.png', RedirectView.as_view(url=app_static('icons/android_chrome_512x512.png'))),
+    path('apple-touch-icon-precomposed.png', RedirectView.as_view(url=app_static('icons/apple_touch_icon_precomposed.png'))),
+    path('apple-touch-icon.png', RedirectView.as_view(url=app_static('icons/apple_touch_icon.png'))),
+    path('browserconfig.xml', RedirectView.as_view(url=app_static('icons/browserconfig.xml'))),
+    path('favicon-16x16.png', RedirectView.as_view(url=app_static('icons/favicon_16x16.png'))),
+    path('favicon-32x32.png', RedirectView.as_view(url=app_static('icons/favicon_32x32.png'))),
+    path('favicon.ico', RedirectView.as_view(url=app_static('icons/favicon.ico'))),
+    path('mstile-150x150.png', RedirectView.as_view(url=app_static('icons/mstile_150x150.png'))),
+    path('safari-pinned-tab.svg', RedirectView.as_view(url=app_static('icons/safari_pinned_tab.svg'))),
 
     path('stackpile', views.StackpileView.as_view(), name='stackpile'),
     path('spooky', views.SpookyView.as_view(), name="spooky"),
