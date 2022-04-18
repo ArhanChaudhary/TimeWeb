178086430cf0495a9c24981e3baf70e722190656
Arhan Chaudhary <arhan.ch@gmail.com>
2022-04-17 20:36:20 -0700

    Update AddSiteID middleware structure

--- timeweb/misc/middleware/AddSiteID.py
+++ timeweb/misc/middleware/AddSiteID.py
@@ -1,13 +1,16 @@
 from django.conf import settings
 from django.contrib.sites.models import Site
-from django.utils.deprecation import MiddlewareMixin
 
 try:
     current_site = Site.objects.get(domain="localhost" if settings.DEBUG else "timeweb.io")
 except Site.DoesNotExist:
     current_site = Site.objects.get(domain="example.com")
 
-class AddSiteID(MiddlewareMixin):
-    def process_request(self, request):
+class AddSiteID:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
         request.current_site = current_site
         settings.SITE_ID = current_site.id
+        return self.get_response(request)
