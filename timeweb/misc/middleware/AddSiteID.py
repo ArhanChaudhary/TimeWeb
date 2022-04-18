2e9f71903925fad5d9bdc1c96a5ccd175cffbe04
Arhan Chaudhary <arhan.ch@gmail.com>
2022-04-17 20:37:10 -0700

    Move 00bbf7d to __init__

--- timeweb/misc/middleware/AddSiteID.py
+++ timeweb/misc/middleware/AddSiteID.py
@@ -1,16 +1,15 @@
 from django.conf import settings
 from django.contrib.sites.models import Site
 
-try:
-    current_site = Site.objects.get(domain="localhost" if settings.DEBUG else "timeweb.io")
-except Site.DoesNotExist:
-    current_site = Site.objects.get(domain="example.com")
-
 class AddSiteID:
     def __init__(self, get_response):
         self.get_response = get_response
+        try:
+            self.current_site = Site.objects.get(domain="localhost" if settings.DEBUG else "timeweb.io")
+        except Site.DoesNotExist:
+            self.current_site = Site.objects.get(domain="example.com")
+        settings.SITE_ID = self.current_site.id
 
     def __call__(self, request):
-        request.current_site = current_site
-        settings.SITE_ID = current_site.id
+        request.current_site = self.current_site
         return self.get_response(request)
