cdf803b106c0578ef9c4867223a58dae413c3bac
Arhan Chaudhary <arhan.ch@gmail.com>
2022-04-21 05:48:37 -0700

    misc isn't necessary in INSTALLED_APPS

--- timeweb/timeweb/settings.py
+++ timeweb/timeweb/settings.py
@@ -75,7 +75,6 @@ INSTALLED_APPS = [
     'timewebauth',
     'timewebapp',
     'navbar',
-    'misc',
     'multiselectfield',
     'django.contrib.admin', # admin needs to be after 'timewebapp' for some reason I forgot but it needs to be here
     'pwa',
