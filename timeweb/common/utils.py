b6072c79146d20dd36d14d3336c15802faba720e
Arhan Chaudhary <arhan.ch@gmail.com>
2022-04-26 18:57:10 -0700

    Move app_static_factory from common.urls to common.utils

--- timeweb/common/utils.py
+++ timeweb/common/utils.py
@@ -11,6 +11,11 @@ def utc_to_local(request, utctime):
     else:
         return timezone.localtime(utctime)
 
+def app_static_factory(app_name):
+    def app_static(url_path):
+        return f"/static/{app_name}/{url_path}" if settings.DEBUG else f'https://storage.googleapis.com/twstatic/{app_name}/{url_path}'
+    return app_static
+
 try:
     current_site = Site.objects.get(domain="localhost" if settings.DEBUG else "timeweb.io")
 except Site.DoesNotExist:
