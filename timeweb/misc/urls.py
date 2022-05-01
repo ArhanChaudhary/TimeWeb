81a8e22b1c565d3801803dfa2778e4391bf315e3
Arhan Chaudhary <arhan.ch@gmail.com>
2022-04-30 18:56:50 -0700

    Use webp instead of png

--- timeweb/misc/urls.py
+++ timeweb/misc/urls.py
@@ -13,15 +13,15 @@ Expires: 2023-07-27T07:00:00.000Z
 Preferred-Languages: en
 Canonical: https://timeweb.io/.well-known/security.txt''', content_type="text/plain"), name="security_file"),
 
-    path('android-chrome-192x192.png', RedirectView.as_view(url=app_static('icons/android_chrome_192x192.png'))),
-    path('android-chrome-512x512.png', RedirectView.as_view(url=app_static('icons/android_chrome_512x512.png'))),
-    path('apple-touch-icon-precomposed.png', RedirectView.as_view(url=app_static('icons/apple_touch_icon_precomposed.png'))),
-    path('apple-touch-icon.png', RedirectView.as_view(url=app_static('icons/apple_touch_icon.png'))),
+    path('android-chrome-192x192.png', RedirectView.as_view(url=app_static('icons/android_chrome_192x192.webp'))),
+    path('android-chrome-512x512.png', RedirectView.as_view(url=app_static('icons/android_chrome_512x512.webp'))),
+    path('apple-touch-icon-precomposed.png', RedirectView.as_view(url=app_static('icons/apple_touch_icon_precomposed.webp'))),
+    path('apple-touch-icon.png', RedirectView.as_view(url=app_static('icons/apple_touch_icon.webp'))),
     path('browserconfig.xml', RedirectView.as_view(url=app_static('icons/browserconfig.xml'))),
-    path('favicon-16x16.png', RedirectView.as_view(url=app_static('icons/favicon_16x16.png'))),
-    path('favicon-32x32.png', RedirectView.as_view(url=app_static('icons/favicon_32x32.png'))),
+    path('favicon-16x16.png', RedirectView.as_view(url=app_static('icons/favicon_16x16.webp'))),
+    path('favicon-32x32.png', RedirectView.as_view(url=app_static('icons/favicon_32x32.webp'))),
     path('favicon.ico', RedirectView.as_view(url=app_static('icons/favicon.ico'))),
-    path('mstile-150x150.png', RedirectView.as_view(url=app_static('icons/mstile_150x150.png'))),
+    path('mstile-150x150.png', RedirectView.as_view(url=app_static('icons/mstile_150x150.webp'))),
     path('safari-pinned-tab.svg', RedirectView.as_view(url=app_static('icons/safari_pinned_tab.svg'))),
 
     path('stackpile', RedirectView.as_view(url="https://stackpile.me"), name='stackpile'),
