1ff1dcc76d8c165991d3453c018d0d6fcf99c133
Arhan Chaudhary <arhan.ch@gmail.com>
2022-05-02 04:45:44 -0700

    Move update_seen_latest_changelog.py to timeweb/utils.py

--- timeweb/update_seen_latest_changelog.py
+++
@@ -1,9 +0,0 @@
-"""
-This file is manually invoked so every user is notified of the latest changelog
-"""
-from navbar.models import SettingsModel
-settings_models = SettingsModel.objects.all()
-for model in settings_models:
-    if model.seen_latest_changelog and not model.enable_tutorial:
-        model.seen_latest_changelog = False
-SettingsModel.objects.bulk_update(settings_models, ['seen_latest_changelog'])
--- timeweb/utils.py
+++ timeweb/utils.py
@@ -16,6 +16,16 @@ def app_static_factory(app_name):
         return f"/static/{app_name}/{url_path}" if settings.DEBUG else f'https://storage.googleapis.com/twstatic/{app_name}/{url_path}'
     return app_static
 
+def update_seen_latest_changelog():
+    """
+    This function is manually invoked so every user is notified of the latest changelog
+    """
+    from navbar.models import SettingsModel
+    settings_models = SettingsModel.objects.all()
+    for model in settings_models:
+        if model.seen_latest_changelog and not model.enable_tutorial:
+            model.seen_latest_changelog = False
+    SettingsModel.objects.bulk_update(settings_models, ['seen_latest_changelog'])
 
 try:
     current_site = Site.objects.get(domain="localhost" if settings.DEBUG else "timeweb.io")
