6962c978b1a75fd89c8d443ecaa400452e2f3d94
Arhan Chaudhary <arhan.ch@gmail.com>
2022-05-29 21:31:14 -0700

    Revert 04be91f with a fix

--- timeweb/timewebapp/static/timewebapp/js/utils.js
+++ timeweb/timewebapp/static/timewebapp/js/utils.js
@@ -1037,8 +1037,7 @@ utils = {
         },
         fixBrowserInconsistencies: function() {
             $(window).one("load", function() {
-                // Still really inconsistent, I'll keep as legacy
-                // $("#assignments-container").css("margin-right", $("#assignments-container").width() - $("main").width());
+                $("#assignments-container").css("margin-right", Math.max(0, $("#assignments-container").width() - $("main").width()));
             });
         },
     },
