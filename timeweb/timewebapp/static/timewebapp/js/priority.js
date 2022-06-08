ArhanChaudhary/TimeWeb

commit b2760479366b8ddff479c58902dfcfc0f1d077ff

Rework <main> and #assignments-container positioning

timeweb/timewebapp/static/timewebapp/js/priority.js
@@ -795,13 +795,13 @@ class Priority {
                         }, 0);
 
                         let scrollTimeout = setTimeout(resolve, 200);
-                        $("main").scroll(() => {
+                        $("#assignments-container").scroll(() => {
                             clearTimeout(scrollTimeout);
                             scrollTimeout = setTimeout(resolve, 200);
                         });
                     });
                 }).then(function() {
-                    $("main").off('scroll');
+                    $("#assignments-container").off('scroll');
                     that.colorOrAnimateInAssignment({
                         dom_assignment,
                         mark_as_done: priority_data.mark_as_done,

timeweb/common/static/common/css/app_base.css
@@ -63,18 +63,12 @@ header > * {
 main {
     position: relative; /* Needed for positioning the background image */
     height: 100vh;
-    padding: 80px 20px 0 20px;
+    padding: 0 20px;
+    padding-top: 70px;
     box-shadow: inset 20px 0 0 0 var(--primary), inset -20px 0 0 0 var(--primary);
     box-sizing: border-box;
     /* some js focuses on the main, causing it to get an outline even with tabindex -1; hide it */
     outline: 0 !important;
-}
-/* Position content such that the scrollbar doesn't clip into the header */
-main.finished-header-transition {
-    overflow-y: overlay !important;
-    height: calc(100vh - 70px);
-    padding: 10px 20px;
-    margin-top: 70px !important;
 }
 #background-image {
     bottom: 0;

timeweb/timewebapp/static/timewebapp/css/app.css
@@ -4,15 +4,17 @@ html {
 }
 #assignments-container {
     position: relative;
-    /* Ensure things dont get cutoff when crud.js puts overflow: hidden on this when an assignment is being deleted */
-    top: -10px;
-    padding-top: 10px;
+    overflow-y: auto;
     opacity: 0;
     --scale-percent-y: 1.05;
     --vertical-assignment-padding: 33px;
     --horizontal-assignment-padding: 15px;
     --outer-assignment-vertical-padding: 8px;
     --outer-assignment-horizontal-padding: 25px;
+}
+main.finished-header-transition #assignments-container {
+    /* ensure the initial transition visual doesnt break */
+    height: 100%;
 }
 main:not(.animate) {
     transition: box-shadow 1.5s cubic-bezier(.32,1.01,.53,1.15);
@@ -43,6 +45,7 @@ header.animate::after {
     position: relative;
     font-size: 17px;
     z-index: 1001;
+    margin-top: 10px;
     padding: 0 var(--outer-assignment-horizontal-padding);
     display: flex;
     align-items: center;

timeweb/timewebapp/static/timewebapp/js/crud.js
@@ -182,7 +182,6 @@ class Crud {
             $("#overlay").fadeIn(Crud.FORM_ANIMATION_DURATION).find("#form-wrapper").animate({top: Crud.FORM_POSITION_TOP}, Crud.FORM_ANIMATION_DURATION);
             $("form input:visible").first().focus();
         }
-        $("main").css("overflow-y", "hidden");
         that.old_unit_value = undefined;
         that.replaceUnit();
 
@@ -209,8 +208,6 @@ class Crud {
                 Crud.GO_TO_FIELD_GROUP({standard: true});
             }).find("#form-wrapper").animate({top: 0}, Crud.FORM_ANIMATION_DURATION);
         }
-        // Fallback if "overlay" doesn't exist
-        $("main").css("overflow-y", "scroll");
     }
     replaceUnit() {
         const that = this;

timeweb/timewebapp/static/timewebapp/js/utils.js
@@ -697,7 +697,7 @@ utils = {
                         }, 0);
                         break;
                     case "t":
-                        $("main").scrollTop(0);
+                        $("#assignments-container").scrollTop(0);
                         break;
                     case "e":
                     case "d":
@@ -979,7 +979,7 @@ utils = {
 
 
             // Saves current open assignments and scroll position to localstorage and sessionstorage if refreshed or redirected
-            // Use beforeunload instead of unload or else the loading screen triggers and $("main").scrollTop() becomes 0
+            // Use beforeunload instead of unload or else the loading screen triggers and $("#assignments-container").scrollTop() becomes 0
             $(window).on('beforeunload', function() {
                 if (!SETTINGS.enable_tutorial) {
                     // Save current open assignments
@@ -989,7 +989,7 @@ utils = {
                         }).toArray()
                     ));
                     // Save scroll position
-                    localStorage.setItem("scroll", $("main").scrollTop());
+                    localStorage.setItem("scroll", $("#assignments-container").scrollTop());
                 }
                 // Send ajax before close if it's on timeout
                 if (ajaxUtils.sendAttributeAjax.assignments.length) {
@@ -1013,7 +1013,7 @@ utils = {
                         // Scroll to original position
                         // Needs to scroll after assignments are opened
                         if ("scroll" in localStorage) {
-                            $("main").scrollTop(localStorage.getItem("scroll"));
+                            $("#assignments-container").scrollTop(localStorage.getItem("scroll"));
                             localStorage.removeItem("scroll");
                         }
                     }, 0);

commit ee718f7cf85fb838c6d79ef07bbb93240329e4b4

b276047 accidentally broke the #animate-in transition

timeweb/timewebapp/static/timewebapp/js/priority.js
@@ -874,7 +874,7 @@ class Priority {
             document.fonts.ready.then(function() {
                 $("#animate-in").css({
                     position: "",
-                    top: $("#assignments-container").offset().top + $("#assignments-container").outerHeight() - $("#animate-in").offset().top + Priority.ANIMATE_IN_START_MARGIN,
+                    top: $(".assignment-container:last").offset().top + $(".assignment-container:last").height() - $("#animate-in").offset().top + Priority.ANIMATE_IN_START_MARGIN,
                     marginBottom: -$("#animate-in").outerHeight(),
                 });
             });