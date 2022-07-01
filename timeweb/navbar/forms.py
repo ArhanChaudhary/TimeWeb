ArhanChaudhary/TimeWeb

commit 022bf70af1902e2f35646611e67dd81e3371763a

Make restore_gc_assignments a non-model settings field

timeweb/navbar/forms.py
@@ -14,6 +14,10 @@ class CustomImageFieldWidget(ClearableFileInput):
     template_name = 'navbar/widgets/clearable_file_input.html'
 
 class SettingsForm(forms.ModelForm):
+    restore_gc_assignments = forms.BooleanField(
+        label="Restore Deleted Google Classroom Assignments",
+    )
+
     class Meta:
         model = SettingsModel
         fields = "__all__"
@@ -53,7 +57,6 @@ class SettingsForm(forms.ModelForm):
             "default_dropdown_tags": "These will show up by default in the tag add dropdown. Separate each default tag with a comma or new line.",
             "animation_speed": "Controls the speed of most animations.",
             "timezone": "Backend calculations use your browser's timezone. If your browser doesn't imply your timezone, choose your timezone here.",
-            "restore_gc_assignments": "Google Classroom assignments are normally added only once. Enabling this adds every Google Classroom assignment again in case you want them back.",
             # "enable_tutorial": "You will also be given the option to enable or disable notifications after enabling this.",
         }
     def __init__(self, *args, **kwargs):

timeweb/navbar/migrations/0006_remove_settingsmodel_restore_gc_assignments.py
@@ -1,0 +1,17 @@
+# Generated by Django 3.2.13 on 2022-07-01 19:44
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('navbar', '0005_alter_settingsmodel_appearance'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='settingsmodel',
+            name='restore_gc_assignments',
+        ),
+    ]

timeweb/navbar/models.py
@@ -149,10 +149,6 @@ class SettingsModel(models.Model):
         null=True,
         blank=True,
     )
-    restore_gc_assignments = models.BooleanField(
-        default=False,
-        verbose_name=_('Restore Deleted Google Classroom Assignments'),
-    )
     enable_tutorial = models.BooleanField(
         default=True,
         verbose_name=_('Tutorial'),

commit f84bf230565a8475e9166bc7b666e56bc34f9cdf

022bf70 accidentally invalidated changing the assignment sorting field

timeweb/navbar/forms.py
@@ -16,6 +16,7 @@ class CustomImageFieldWidget(ClearableFileInput):
 class SettingsForm(forms.ModelForm):
     restore_gc_assignments = forms.BooleanField(
         label="Restore Deleted Google Classroom Assignments",
+        required=False,
     )
 
     class Meta: