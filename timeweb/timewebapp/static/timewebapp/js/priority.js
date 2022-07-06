c6866e286b8404efbe9d56e2ecb2d18a289f2b22
Arhan Chaudhary <arhan.ch@gmail.com>
2022-07-06 14:23:18 -0700

    Needs more info assignments are now marked as completely finished when their due dates pass

--- timeweb/timewebapp/static/timewebapp/js/priority.js
+++ timeweb/timewebapp/static/timewebapp/js/priority.js
@@ -230,7 +230,7 @@ class Priority {
 
             let alert_due_date_passed_cond = false;
             let status_value, status_message, status_image, due_date_minus_today;
-            if (sa.sa.needs_more_info && !(sa.sa.is_google_classroom_assignment && due_date_passed && !sa.sa.soft)) {
+            if (sa.sa.needs_more_info && !(due_date_passed && !sa.sa.soft)) {
                 status_image = 'question_mark';
                 if (sa.sa.is_google_classroom_assignment) {
                     status_message = "This Google Classroom assignment needs more info";
