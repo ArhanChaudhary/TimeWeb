a1cc5921ff055d3670f7a73c7487110287d2a5d0
Arhan Chaudhary <arhan.ch@gmail.com>
2022-07-15 02:37:48 -0700

    Further test and polish up the outdated service workers bug

--- timeweb/timewebapp/static/timewebapp/js/serviceworker.js
+++ timeweb/timewebapp/static/timewebapp/js/serviceworker.js
@@ -1,4 +1,10 @@
+// message to future self stop panicking about the outdated service worker bug
+// ive tested it like 20 times and the result has always been that it gets fixed
+// after the next reload
 self.addEventListener('fetch', function() {
     return;
 });
 self.skipWaiting();
+// self.clients.claim() doesn't take over the last service worker fast enough rip
+// it doesn't work if called before skipWaiting() either
+// self.clients.claim();
