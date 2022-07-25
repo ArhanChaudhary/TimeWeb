ArhanChaudhary/TimeWeb

commit 09d8fa4f18d1ac48f9006ff7e5373c4859933112

Assignment style for deleted assignments view

timeweb/timewebapp/static/timewebapp/js/graph.js
@@ -95,10 +95,11 @@ class Assignment {
     getWorkingDaysRemaining(params={reference: null, floor_due_time: false}) {
         const original_red_line_start_x = this.red_line_start_x;
         switch (params.reference) {
-            case "today":
+            case "today": {
                 let today_minus_assignment_date = mathUtils.daysBetweenTwoDates(date_now, this.sa.assignment_date);
                 this.red_line_start_x = today_minus_assignment_date;
                 break;
+            }
             case "blue line end": {
                 let len_works = this.sa.works.length - 1;
                 this.red_line_start_x = this.sa.blue_line_start + len_works;
@@ -1098,7 +1099,7 @@ class VisualAssignment extends Assignment {
     positionTags() {
         const dom_tags = this.dom_assignment.find(".tags");
         switch (SETTINGS.horizontal_tag_position) {
-            case "Left":
+            case "Left": {
                 if (SETTINGS.vertical_tag_position === "Top") {
                     dom_tags.removeClass("tags-top");
                     dom_tags.addClass("tags-bottom");
@@ -1130,12 +1131,14 @@ class VisualAssignment extends Assignment {
                     dom_tags.removeClass("tags-bottom");
                 }
                 break;
+            }
 
-            case "Middle":
+            case "Middle": {
                 if (!dom_tags.parents(".align-to-status-message-container").length) {
                     dom_tags.prependTo(this.dom_assignment.find(".align-to-status-message-container"));
                 }
                 break;
+            }
         }
     }
     displayTruncateWarning() {

timeweb/timewebapp/static/timewebapp/css/app.css
@@ -61,6 +61,9 @@ main.background-image-exists {
 #assignments-container.viewing-deleted-assignments .graph-footer {
     height: var(--assignment-margin-bottom);
 }
+#assignments-container.viewing-deleted-assignments .left-side-of-header {
+    --left-side-of-header-width: initial;
+}
 #extra-navs-positioner {
     box-sizing: border-box;
     min-height: calc(100vh - var(--header-height) - 10px);

timeweb/timewebapp/templates/timewebapp/app.html
@@ -226,21 +226,23 @@
                     <div class="description">{{ assignment.description|linebreaks }}</div>
                   {% endif %}
                 </div>
-                <svg width="30px" height="20px" class="arrow-container">
-                  <polygon points="5,0 5,20 15,10" fill="currentColor">
-                    <animate class="rising-arrow-animation" begin="indefinite" fill="freeze" attributeName="points" dur="100ms" to="20,5 0,5 10,15"></animate>
-                    <animate class="falling-arrow-animation" begin="indefinite" fill="freeze" attributeName="points" dur="100ms" to="5,0 5,20 15,10"></animate>
-                  </polygon>
-                </svg>
-                <div class="right-side-of-header">
-                  <svg class="status-image" preserveAspectRatio="none">
-                    <use></use>
+                {% if not view_deleted_assignments_in_app_view %}
+                  <svg width="30px" height="20px" class="arrow-container">
+                    <polygon points="5,0 5,20 15,10" fill="currentColor">
+                      <animate class="rising-arrow-animation" begin="indefinite" fill="freeze" attributeName="points" dur="100ms" to="20,5 0,5 10,15"></animate>
+                      <animate class="falling-arrow-animation" begin="indefinite" fill="freeze" attributeName="points" dur="100ms" to="5,0 5,20 15,10"></animate>
+                    </polygon>
                   </svg>
-                  <div class="align-to-status-message-container">
-                    <span class="status-message"></span>
+                  <div class="right-side-of-header">
+                    <svg class="status-image" preserveAspectRatio="none">
+                      <use></use>
+                    </svg>
+                    <div class="align-to-status-message-container">
+                      <span class="status-message"></span>
+                    </div>
                   </div>
-                  <span class="completion-time"></span>
-                </div>
+                {% endif %}
+                <span class="completion-time"></span>
                 {% if not view_deleted_assignments_in_app_view %}
                   {% if assignment.google_classroom_assignment_link %}
                     <a target="_blank" rel="noopener" href="{{ assignment.google_classroom_assignment_link }}" title="Assignment Link" class="assignment-header-button hide-on-mobile">

commit a4ede5fa41c82cfc585345fba0494eed620d0a99

09d8fa4 accidentally broke the graph

timeweb/timewebapp/static/timewebapp/js/graph.js
@@ -1186,7 +1186,7 @@ $(".assignment").click(function(e/*, params={ initUI: true }*/) {
             sa.positionTags();
         });
         sa.positionTags();
-        dom_assignment.find(".falling-arrow-animation")[0].beginElement();
+        dom_assignment.find(".falling-arrow-animation")[0]?.beginElement();
         return;
     }
     SETTINGS.one_graph_at_a_time && $(".assignment.open-assignment").click();
@@ -1197,7 +1197,7 @@ $(".assignment").click(function(e/*, params={ initUI: true }*/) {
     sa.positionTags();
     sa.displayTruncateWarning();
     assignment_footer.css("display", "block");
-    dom_assignment.find(".rising-arrow-animation")[0].beginElement();
+    dom_assignment.find(".rising-arrow-animation")[0]?.beginElement();
     sa.initUI();
     (() => {
         if (!SETTINGS.enable_tutorial || already_ran_tutorial) return;