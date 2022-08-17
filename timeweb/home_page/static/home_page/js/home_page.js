40fd01e553448843afc717a51a8c1460ca3d03de
Arhan Chaudhary <arhan.ch@gmail.com>
2022-08-16 22:03:57 -0700

    circle scroller effect

--- timeweb/home_page/static/home_page/js/home_page.js
+++ timeweb/home_page/static/home_page/js/home_page.js
@@ -1,58 +1,40 @@
-function getFirstSectionMid() {
-    return $(".section-block#first").height() / 2 + 10;
+function scalingFunction(x) {
+    // these numbers were not chosen randomly! https://www.desmos.com/calculator/sxvbuzk0ps
+    if (x < 0.02) return x * 5 + 0.1;
+    return Math.sqrt(1 - Math.pow((x - 1) / 1, 2));
+}
+function clamp(low, value, high) {
+    return Math.min(Math.max(value, low), high)
 }
 function setMoveLefts() {
-    const section_mid = getFirstSectionMid();
+    const section_mid = $(".section-block#first").height() / 2;
     $(".assignment-scroller-image").each(function() {
         if ($(this).offset().top + $(this).height() > 0 && $(this).offset().top < $("#second").offset().top) {
             const mid = $(this).offset().top + $(this).height() / 2;
-            const scaled_factor = Math.abs(section_mid - mid) / section_mid - 1;
-            this.parentElement.style.setProperty("--move-left", Math.min(0, scaled_factor * 90) + "px");
-            // $(this).css("margin-left", factor * 10);
+            let linear_factor = 1 - Math.abs(section_mid - mid) / section_mid;
+            const scaled_factor = -scalingFunction(linear_factor);
+            this.parentElement.style.setProperty("--move-left", scaled_factor * 150 - 100 + "px");
         } else {
             this.parentElement.style.setProperty("--move-left", "0px");
         }
     });
 }
-function scrollDown() {
-    if ($("#second")[0].getBoundingClientRect().y < 0) return;
-
-    const most_left = $(".assignment-scroller-image-extender").toArray().reduce((prev, current) => parseFloat(window.getComputedStyle(current).getPropertyValue("--move-left")) < parseFloat(window.getComputedStyle(prev).getPropertyValue("--move-left")) ? current : prev);
-    const mid = $(most_left).offset().top + $(most_left).height() / 2;
-
-    let move_up = $(most_left).find(".assignment-scroller-image").outerHeight(true);
-    // correction (centering the image)
-    move_up += mid - getFirstSectionMid();
-    setTimeout(() => {
-        $(".assignment-scroller-image-extender").each(function() {
-            $(this).toggleClass("hover", $(this).prev().is(most_left));
-        });
-    }, 200);
-    $(".section-block#first .right-section-side").animate({
-        textIndent: "-=" + move_up,
-    }, {
-        duration: 500,
-        step: function(now) {
-            this.style.setProperty("--move-up", now + "px");
-            setMoveLefts();
-        },
-        complete: function() {
-            const last = $(".assignment-scroller-image-extender").last();
-            if (last.offset().top + last.height() < $(".section-block#first").offset().top + $(".section-block#first").height()) {
-                $(this).css("text-indent", "0px");
-                this.style.setProperty("--move-up", "0px");
-                setMoveLefts();
-            }
-        }
-    });
-}
 $(window).one("load", function() {
-    setMoveLefts();
-    scrollDown();
-    setInterval(scrollDown, 2000);
+
+    now = 0;
+    const last = $(".assignment-scroller-image-extender").last();
+    const container = $(".section-block#first .right-section-side").prop("style")
+    setInterval(() => {
+        container.setProperty("--move-up", now + "px");
+        setMoveLefts();
+        now -= 1;
+        if (last.offset().top + last.height() < $(".section-block#first").offset().top + $(".section-block#first").height()) {
+            now = 0;
+        }
+    }, 20);
+    turn_mod = window.getComputedStyle($("#hour-hand")[0]).getPropertyValue("--turns-per-turn");
 });
 let turns = 0;
-let turn_mod = window.getComputedStyle($("#hour-hand")[0]).getPropertyValue("--turns-per-turn");
 $(window).scroll(function() {
     $("#favicon-animated").prop("style").setProperty("--turns", turns);
     turns += 0.05;
