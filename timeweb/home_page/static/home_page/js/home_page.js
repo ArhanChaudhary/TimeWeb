f9aded079d792ddf1e8dd5b6d8f42d688a2666e5
Arhan Chaudhary <arhan.ch@gmail.com>
2022-08-19 00:13:12 -0700

    Use transform only to animate the assignments scroller

--- timeweb/home_page/static/home_page/js/home_page.js
+++ timeweb/home_page/static/home_page/js/home_page.js
@@ -7,11 +7,12 @@ function clamp(low, value, high) {
     return Math.min(Math.max(value, low), high)
 }
 scaled_horizontal_factor = 0;
+now = 0;
 function setMoveLefts() {
     const section_mid = $(".section-block#first").height() / 2;
     $(".assignment-scroller-image").each(function() {
         if ($(this).offset().top + $(this).height() > 0 && $(this).offset().top < $("#second").offset().top) {
-            const mid = $(this).offset().top + $(this).height() / 2;
+            let mid = $(this).offset().top + $(this).height() / 2;
             let linear_factor = 1 - Math.abs(section_mid - mid) / section_mid;
             if (linear_factor < 0.5) {
                 var opacity = linear_factor * 1.2 + 0.3;
@@ -20,30 +21,27 @@ function setMoveLefts() {
             }
             $(this).css("opacity", opacity);
             const scaled_factor = -scalingFunction(linear_factor);
-            this.parentElement.style.setProperty("--move-left", scaled_factor * scaled_horizontal_factor - 200 + "px");
+            this.parentElement.style.setProperty("--move-left", scaled_factor * scaled_horizontal_factor + "px");
         } else {
             this.parentElement.style.setProperty("--move-left", "0px");
         }
     });
 }
 $(window).one("load", function() {
-
     const last = $(".assignment-scroller-image-extender").last();
     const container = $(".section-block#first .right-section-side").prop("style");
     setMoveLefts();
     $(".section-block#first .right-section-side").addClass("animate");
-    let now = 0;
-    let diff = 7;
+    let diff = 0.2;
     let diff_diff = 0.07;
     let scaled_horizontal_factor_diff = 4.6;
-    a = new Date();
-    setInterval(() => {
+    function step() {
         container.setProperty("--move-up", now + "px");
         setMoveLefts();
         now -= diff;
-        if (diff > 1.5) {
-            diff -= diff_diff;
-            diff_diff -= 0.0003;
+        if (diff < 1.5) {
+            diff += diff_diff;
+            diff_diff += 0.0003;
         }
         if (scaled_horizontal_factor < 150) {
             scaled_horizontal_factor += scaled_horizontal_factor_diff;
@@ -53,7 +51,9 @@ $(window).one("load", function() {
         if (last.offset().top + last.height() < $(".section-block#first").offset().top + $(".section-block#first").height()) {
             now = 0;
         }
-    }, 30);
+        requestAnimationFrame(step);
+    }
+    requestAnimationFrame(step);
     turn_mod = window.getComputedStyle($("#hour-hand")[0]).getPropertyValue("--turns-per-turn");
 });
 let turns = 0;
