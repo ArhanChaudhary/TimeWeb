eee661244e763ff08cfdf4a5dd99ed7c4ac7c000
Arhan Chaudhary <arhan.ch@gmail.com>
2022-09-22 17:40:13 -0700

    Minor reword

--- timeweb/navbar/forms.py
+++ timeweb/navbar/forms.py
@@ -94,7 +94,7 @@ class SettingsForm(forms.ModelForm):
             "animation_speed": "Controls the speed of most animations.",
             "timezone": "Backend calculations use your browser's timezone. If your browser doesn't imply your timezone, choose your timezone here.",
             "sorting_animation_threshold": "Only do the assignment sorting animation when there are this many assignments or less. Due to performance lag as the number of assignments increase, enter a higher number if your device is high-end and a lower number if your device is low-end.",
-            "immediately_delete_completely_finished_assignments": "Immediately delete assignments that are completely finished (marked with a star icon). Deleted assignments can be recovered and restored from the deleted assignments view.",
+            "immediately_delete_completely_finished_assignments": "Immediately delete assignments that are completely finished by your work inputs. Ignores assignments that are marked as completely finished from their due dates passing. Deleted assignments can be recovered and restored from the deleted assignments view.",
             # "enable_tutorial": "You will also be given the option to enable or disable notifications after enabling this.",
         }
     def __init__(self, *args, **kwargs):
