ec9104a37c1fd7f80711d090ca2fdb2c460c5070
Arhan Chaudhary <arhan.ch@gmail.com>
2022-11-18 09:42:58 -0800

    Shake assignments that can't be opened in the deleted assignments view

--- timeweb/timewebapp/static/timewebapp/js/graph.js
+++ timeweb/timewebapp/static/timewebapp/js/graph.js
@@ -1381,6 +1381,14 @@ class VisualAssignment extends Assignment {
     canOpenAssignment() {
         return !this.sa.needs_more_info;
     }
+    shake() {
+        const that = this;
+        that.dom_assignment.animate({left: -5}, 75, "easeOutCubic", function() {
+            that.dom_assignment.animate({left: 5}, 75, "easeOutCubic", function() {
+                that.dom_assignment.animate({left: 0}, 75, "easeOutCubic");
+            });
+        });
+    }
 }
 window.VisualAssignment = VisualAssignment;
 let already_ran_tutorial = false;
@@ -1398,7 +1406,8 @@ $(".assignment").click(function(e/*, params={ initUI: true }*/) {
     const sa = new VisualAssignment(dom_assignment);
     
     if (!sa.canOpenAssignment()) {
-        dom_assignment.find(".update-button").click();
+        if (dom_assignment.find(".update-button").click().length === 0)
+            sa.shake();
         return;
     }
 
