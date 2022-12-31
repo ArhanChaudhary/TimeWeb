9331fa565d14a5996ee38cc6e59c03c2540c028d
Arhan Chaudhary <arhan.ch@gmail.com>
2022-12-30 22:56:05 -0600

    Reword timezone setting field description with respect to 0f15f20

--- timeweb/navbar/forms.py
+++ timeweb/navbar/forms.py
@@ -103,7 +103,7 @@ class SettingsForm(forms.ModelForm):
             "default_dropdown_tags": "These will show up by default in the tag add dropdown. Separate each default tag with a comma or new line.",
             "appearance": "Lesser dark mode doesn't color your assignment titles.",
             "animation_speed": "Controls the speed of most animations.",
-            "timezone": "Backend calculations use your browser's timezone. If your browser doesn't imply your timezone, choose your timezone here.",
+            "timezone": "Backend calculations use your browser's timezone. Choose your timezone here if you wish to change it.",
             "sorting_animation_threshold": "Only do the assignment sorting animation when there are this many assignments or less. Due to performance lag as the number of assignments increase, enter a higher number if your device is high-end and a lower number if your device is low-end.",
             "immediately_delete_completely_finished_assignments": "Immediately delete assignments that are completely finished by your work inputs. Ignores assignments that are marked as completely finished from their due dates passing. Deleted assignments can be recovered and restored from the deleted assignments view.",
             # "enable_tutorial": "You will also be given the option to enable or disable notifications after enabling this.",
