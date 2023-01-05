f3d406ad8ccf63925f3fcd5d44e0228d4a7d0e48
Arhan Chaudhary <arhan.ch@gmail.com>
2023-01-05 01:19:53 -0800

    /accounts/logout must be available for POST endpoint, only disable the GET endpoint

--- timeweb/timewebauth/urls.py
+++ timeweb/timewebauth/urls.py
@@ -2,14 +2,14 @@ from django.views.defaults import page_not_found
 from django.urls import path, re_path
 from django.conf import settings
 from . import views
+from allauth.account.views import LogoutView
 
 urlpatterns = [
     # Redirect to login instead of password/reset/key/done/ after a successful password reset
     re_path(r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$", views.password_reset_from_key_no_done_view, name="account_reset_password_from_key"),
     path("password/reset/key/done/", page_not_found, {'exception': Exception()}),
-    # There is already a built-in functionality to logout in the app
-    # Remove the original allauth view
-    path('logout/', page_not_found, {'exception': Exception()}),
+    # Remove the original allauth templates
+    path('logout/', LogoutView.as_view(http_method_names=['post']), name='account_logout'),
     # Slight modification to this route's view
     path("social/connections/", views.labeled_connections, name="socialaccount_connections"),
     # Own view
