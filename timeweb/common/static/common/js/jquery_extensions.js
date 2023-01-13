3c4ed69b9081f89af93480f252b99c48f1c60b8c
Arhan Chaudhary <arhan.ch@gmail.com>
2023-01-13 04:35:04 -0800

    Revert e8f6a9f with a question mark

--- timeweb/common/static/common/js/jquery_extensions.js
+++ timeweb/common/static/common/js/jquery_extensions.js
@@ -27,11 +27,13 @@
     }
     $.fn.expandableTextareaHeight = function() {
         $(this).on("input", function() {
-            // Needed for expandinxg with text
+            // TODO: what the heck is e8f6a9f
             // Use 0 instead of "auto" because firefox
-            $(this).css("height", 0);
-
+            // $(this).css("height", 0);
+            
+            $(this).css("height", "auto"); // Needed for expanding with text
             $(this).css("height", this.scrollHeight);
+
         // there's sometimes a random scrollbar
         }).css("overflow-y", "hidden");
         return $(this);
