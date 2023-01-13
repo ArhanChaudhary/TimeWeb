cd9a545d04db957be44e7963f488a11a65b14b0d
Arhan Chaudhary <arhan.ch@gmail.com>
2023-01-13 05:05:12 -0800

    Explain why a possible bug for showing the outdated alert cannot happen

--- timeweb/api/views.py
+++ timeweb/api/views.py
@@ -154,6 +154,8 @@ def change_setting(request):
 
 @require_http_methods(["POST"])
 def evaluate_current_state(request):
+    # For accounts just created, the device_uuid will be None and then go to created_tab_after_last_api_call
+    # Here, the tab creation time will ALWAYS be after the account creation time, even if by barely, so it will return 200
     same_device = request.POST['device_uuid'] == request.user.settingsmodel.device_uuid
     if same_device:
         return HttpResponse(status=200)
