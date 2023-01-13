da3bd681986481658f837eba37f3265aab1ad69b
Arhan Chaudhary <arhan.ch@gmail.com>
2023-01-13 04:23:40 -0800

    Make /contact/sent/ 404

--- timeweb/navbar/contact_form_urls.py
+++ timeweb/navbar/contact_form_urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import forms, views
+from django.views.defaults import page_not_found
 
 urlpatterns = [
     path('', views.SecuredFormView.as_view(form_class=forms.ContactForm), name='secured_contact_form'),
+    path('sent/', page_not_found, {'exception': Exception()}),
 ]
