2ab2391fbcb6bcc171a5c2ccdfcd8bbc23627d3d
Arhan Chaudhary <arhan.ch@gmail.com>
2023-02-11 18:50:24 -0800

    Hide placeholder on work_input_textbox focus

--- timeweb/timewebapp/static/timewebapp/js/graph.js
+++ timeweb/timewebapp/static/timewebapp/js/graph.js
@@ -1323,18 +1323,27 @@ class VisualAssignment extends Assignment {
         // BEGIN Work input textbox
         {
         let enter_fired = false;
-        work_input_textbox.keydown(e => {
+        const work_input_elements = work_input_textbox.add(this.dom_assignment.find(".work-input-unit-of-time-widget"));
+        work_input_textbox.attr("data-placeholder", work_input_textbox.attr("placeholder"));
+        work_input_elements.on("keydown", e => {
             if (e.key === "Enter" && !enter_fired) {
                 enter_fired = true;
                 submit_work_button.click();
             }
-        // If you enter a number and press enter and the assignments sort, focus is removed from work_input_textbox and the keyup returns early
-        // because e.target is now instead body
-        // This can cause enter_fired to remain true and using enter to not work
-
-        // THis ensures that enter_fired is appropriately changed if the assignments re sort
-        }).on("blur", function() {
+        }).on("blur", function(e) {
+            // If you enter a number and press enter and the assignments sort, focus is removed from work_input_textbox and the keyup returns early
+            // because e.target is now instead body
+            // This can cause enter_fired to remain true and using enter to not work
+    
+            // This ensures that enter_fired is appropriately changed if the assignments re sort
             enter_fired = false;
+
+            if (e.target !== e.relatedTarget && $(e.relatedTarget).is(work_input_elements) && $(e.target).is(work_input_elements)) return; 
+            work_input_textbox.attr("placeholder", work_input_textbox.attr("data-placeholder"));
+            // dont remove data-placeholder or it breaks
+        }).on("focus", function(e) {
+            if (e.target !== e.relatedTarget && $(e.relatedTarget).is(work_input_elements) && $(e.target).is(work_input_elements)) return; 
+            work_input_textbox.removeAttr("placeholder");
         });
         // an alert can sometimes cause the enter to be fired on another element, instead listen to the event's propagation to the root
         $(document).keyup(e => {
