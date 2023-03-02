ba4ea12974a42f9160d50170c922a540b6e6ec7f
Arhan Chaudhary <arhan.ch@gmail.com>
2023-03-01 21:37:26 -0800

    Remove doc template dropdowns

--- timeweb/navbar/static/navbar/js/base.js
+++ timeweb/navbar/static/navbar/js/base.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     $(".major-category").reverse().each(function() {
         const $major_category = $(this);
         const major_category_li = $($("#table-of-contents-major-category-template").html());
-        let minor_categories = $major_category.siblings("details");
+        let minor_categories = $major_category.siblings(".minor-category");
 
         if ($major_category.hasClass("dont-create-dropdown")) {
             const $major_category_href_element = $major_category.siblings().filter(function() {
@@ -17,25 +17,25 @@ document.addEventListener("DOMContentLoaded", function() {
             minor_category_li.find("a").attr("href", `#${$major_category_href_element.attr("id")}`).text($major_category.text());
             $major_category_href_element.css("scroll-margin-top", href_scroll_margin);
             $("#table-of-contents-container #category-table-of-contents").after(minor_category_li);
-        } else {
-            if (!minor_categories.length)
-                minor_categories = minor_categories.add($major_category);
+        } else if (!minor_categories.length) {
             major_category_li.find("span").text($major_category.text());
+            const $minor_category_href_element = $major_category.siblings().filter(function() {
+                return !!$(this).attr("id");
+            }).first();
+            $minor_category_href_element.css("scroll-margin-top", href_scroll_margin);
 
+            const minor_category_li = $($("#table-of-contents-minor-category-template").html());
+            minor_category_li.find("a").attr("href", `#${$minor_category_href_element.attr("id")}`)
+                .text($major_category.text())
+            major_category_li.find("ul").append(minor_category_li);
+            $("#table-of-contents-container #category-table-of-contents").after(major_category_li);
+        } else {
+            major_category_li.find("span").text($major_category.text());
             minor_categories.each(function() {
-                let $minor_category = $(this).find(".minor-category");
-                let $minor_category_href_element = $minor_category;
-                if (!$minor_category.length) {
-                    $minor_category = $minor_category.add($major_category);
-                    $minor_category_href_element = $minor_category.siblings().filter(function() {
-                        return !!$(this).attr("id");
-                    }).first();
-                }
-                $minor_category_href_element.css("scroll-margin-top", href_scroll_margin);
-
+                $(this).css("scroll-margin-top", href_scroll_margin);
                 const minor_category_li = $($("#table-of-contents-minor-category-template").html());
-                minor_category_li.find("a").attr("href", `#${$minor_category_href_element.attr("id")}`)
-                    .text($minor_category.text())
+                minor_category_li.find("a").attr("href", `#${$(this).attr("id")}`)
+                    .text($(this).text())
                 major_category_li.find("ul").append(minor_category_li);
             });
             $("#table-of-contents-container #category-table-of-contents").after(major_category_li);
