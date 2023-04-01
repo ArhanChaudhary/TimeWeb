2b789fdfa8dc8a6b2bdf80c632cf26fcdb45b8b8
Arhan Chaudhary <arhan.ch@gmail.com>
2023-04-01 06:09:00 -0700

    First tutorial alert with jump animation

--- timeweb/timewebapp/static/timewebapp/js/utils.js
+++ timeweb/timewebapp/static/timewebapp/js/utils.js
@@ -848,32 +848,25 @@ insertTutorialMessages: function(first_available_assignment) {
     $("#tutorial-click-assignment-to-open").remove();
     if (!SETTINGS.enable_tutorial) return;
 
-    const assignments_excluding_example = $(".assignment").filter(function() {
-        return utils.loadAssignmentData($(this)).name !== EXAMPLE_ASSIGNMENT_NAME;
-    });
-    if (assignments_excluding_example.length) {
-        first_available_assignment.after($("#tutorial-click-assignment-to-open-template").html());
-        if (!utils.ui.alreadyScrolled) {
-            // setTimeout needed because this runs before domSort
-            setTimeout(function() {
-                $("#tutorial-click-assignment-to-open")[0].scrollIntoView({behavior: 'smooth', block: 'nearest'});
-                new Promise(function(resolve) {
-                    let scrollTimeout = setTimeout(resolve, 200);
-                    $("#assignments-container").scroll(() => {
-                        clearTimeout(scrollTimeout);
-                        scrollTimeout = setTimeout(resolve, 200);
-                    });
-                }).then(function() {
-                    $("#assignments-container").off('scroll');
-                    first_available_assignment.focus();
-                });
-            }, 0);
-            utils.ui.alreadyScrolled = true;
+    first_available_assignment.after($("#tutorial-click-assignment-to-open-template").html());
+    $.alert({
+        title: $("#tutorial-animation-template").html(),
+        content: "TimeWeb is a time management application that visualizes, quantifies, and prioritizes your daily school or work assignments. Here's a demonstration of how it works.",
+        backgroundDismiss: false,
+        buttons: {
+            "Skip tutorial": {
+                action: function() {
+                    SETTINGS.enable_tutorial = false;
+                    ajaxUtils.changeSetting({setting: "enable_tutorial", value: SETTINGS.enable_tutorial});
+                }
+            },
+            next: {
+                action: function() {
+
+                }
+            }
         }
-    } else {
-        $("#assignments-header").replaceWith('<div id="tutorial-message" class="grey-highlight"><div>Welcome to TimeWeb! Thank you so much for your interest!</div><br><div>Create your first school or work assignment by clicking the plus icon to get started.</div></div>');
-        $(".assignment-container, #current-date-container").hide();
-    }
+    });
 },
 graphAlertTutorial: function(days_until_due) {
     // jconfirm may return the focus to .assignment after the alert
