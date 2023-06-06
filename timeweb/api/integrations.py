c0a716bad7e8e6c342d806c7d0cfb4c1b73bc345
Arhan Chaudhary <arhan.ch@gmail.com>
2023-06-05 18:47:17 -0700

    Move get_coursework_model_data to a lambda

--- timeweb/api/integrations.py
+++ timeweb/api/integrations.py
@@ -712,20 +712,6 @@ async def create_canvas_assignments(request):
     # django implementation reference: https://github.dev/Harvard-University-iCommons/django-canvas-oauth/tree/master/canvas_oauth
     await sync_to_async(lambda: request.user.settingsmodel)()
     loop = asyncio.get_event_loop()
-    def get_coursework_model_data(course_id):
-        try:
-            if settings.DEBUG:
-                t = time.time()
-                logger.info(f"started for canvas course_id {course_id}")
-            ret = list(Course(canvas._Canvas__requester, {'id': course_id}).get_assignments(
-                order_by='due_at',
-                # bucket=("undated", "upcoming", "future"), doesn't work because canvas api only supports one string parameter for bucket
-            ))
-            if settings.DEBUG:
-                logger.info(f"finished for canvas course_id {course_id} in {time.time() - t}")
-            return ret
-        except ConnectionError:
-            return []
     def filter_response_data_after_now(response_data):
         complete_date_now = timezone.now()
         now_search_left = 0
@@ -748,12 +734,24 @@ async def create_canvas_assignments(request):
         if settings.DEBUG:
             logger.info("started canvas api requests")
             t = time.time()
-        assignment_model_data = [format_response_datum(response_datum) for response_data in asyncio.as_completed([
-            loop.run_in_executor(None, get_coursework_model_data, course_id['id']) for course_id in request.user.settingsmodel.canvas_courses_cache
-        ]) for response_datum in filter_response_data_after_now(await response_data)]
+        assignment_model_data = [
+            assignment_model_datum
+            for response_data in asyncio.as_completed([
+                loop.run_in_executor(None, lambda: list(Course(canvas._Canvas__requester, {'id': course_id['id']}).get_assignments(
+                    order_by='due_at',
+                    # bucket=("undated", "upcoming", "future"), doesn't work because canvas api only supports one string parameter for bucket
+                )))
+                for course_id in request.user.settingsmodel.canvas_courses_cache
+            ])
+            for response_datum in filter_response_data_after_now(await response_data)
+            if (assignment_model_datum := format_response_datum(response_datum)) is not None
+        ]
         if settings.DEBUG:
             logger.info(f"finished all canvas requests in {time.time() - t}")
     except InvalidAccessToken:
+        # do stuff
+        pass
+    except ConnectionError:
         pass
 
 @require_http_methods(["GET"])
