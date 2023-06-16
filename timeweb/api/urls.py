79875b226636e04c8d9819775f73fe357be03159
Arhan Chaudhary <arhan.ch@gmail.com>
2023-06-15 22:45:57 -0700

    Update integration route name

--- timeweb/api/urls.py
+++ timeweb/api/urls.py
@@ -8,21 +8,21 @@ urlpatterns = [
     path("save-assignment/", views.save_assignment, name="save_assignment"),
     path("change-setting/", views.change_setting, name="change_setting"),
     path("evaluate-current-state/", views.evaluate_current_state, name="evaluate_changed_state"),
-    path("create-integration-assignments/", integrations.create_integration_assignments, name="create_gc_assignments"),
-    path("update-integration-courses/", integrations.update_integration_courses, name="update_gc_courses"),
+    path("create-integration-assignments/", integrations.create_integration_assignments, name="create_integration_assignments"),
+    path("update-integration-courses/", integrations.update_integration_courses, name="update_integration_courses"),
     path("gc-auth-callback/", integrations.gc_auth_callback, name="gc_auth_callback"),
     path("canvas-auth-callback/", integrations.canvas_auth_callback, name="canvas_auth_callback"),
 ]
 INCLUDE_IN_STATE_EVALUATION = ("submit_assignment", "delete_assignment",
     "restore_assignment", "save_assignment", "change_setting", )
 CONDITIONALLY_EXCLUDE_FROM_STATE_EVALUATION = (
-    # having create_gc_assignments in this list used to run the risk of having the
+    # having create_integration_assignments in this list used to run the risk of having the
     # "your assignments are outdated" alert to run (see bb9dac2) 
-    # Though this is not rigorously tested, evaluating state for create_gc_assignments
+    # Though this is not rigorously tested, evaluating state for create_integration_assignments
     # *should* not ever product this same undesired alert because state is only
     # being updated when absolutely necessary (creating assignments)
     # it can be thought of as a normal api call that updates state, just a bit
     # delayed
-    "create_gc_assignments", )
-EXCLUDE_FROM_UPDATING_STATE = ("evaluate_changed_state", "update_gc_courses", "gc_auth_callback", "canvas_auth_callback", )
+    "create_integration_assignments", )
+EXCLUDE_FROM_UPDATING_STATE = ("evaluate_changed_state", "update_integration_courses", "gc_auth_callback", "canvas_auth_callback", )
 assert len(INCLUDE_IN_STATE_EVALUATION) + len(CONDITIONALLY_EXCLUDE_FROM_STATE_EVALUATION) + len(EXCLUDE_FROM_UPDATING_STATE) == len(urlpatterns), "update this"
